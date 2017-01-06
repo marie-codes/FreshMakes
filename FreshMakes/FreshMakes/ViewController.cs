873267fd6e801a1ca4240cbc79d0d831a37235e7
mariejinpark <mariejinpark@gmail.com>
2017-01-06 06:54:30 -0500

    tableView and twitter user timeline populating

--- FreshMakes/FreshMakes/ViewController.cs
+++ FreshMakes/FreshMakes/ViewController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-
 using UIKit;
+using Tweetinvi;
+using System.Collections.Generic;
 
 namespace FreshMakes
 {
@@ -8,13 +9,45 @@ namespace FreshMakes
 	{
 		protected ViewController(IntPtr handle) : base(handle)
 		{
-			// Note: this .ctor should not contain any initialization logic.
 		}
 
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
+			Auth.SetUserCredentials("mJTwVjbfg4GUs0S3j1RN7GUMa", "KcDFpnQ6JbYyayUxNX8RURr4gVW4OdGto5i9dw8Fje9frQDpzM", "364491875-zyI5q3bO8fcauBf7GwpdOvtCKtapzYHYCkFJPyRJ", "yKvUzVyGZgoxCfrgIwA4fjIrmrJ1EX5oCQR04VhUxRrxE");
+			var tweets = Tweetinvi.Search.SearchTweets("from:FreshDirect");
+			List<Tuple<string, string>> myCollection = new List<Tuple<string, string>>();
+
+			foreach (var tweet in tweets)
+			{
+				var text = tweet.Text;
+				string url = null;
+				if (tweet.Media.Count > 0)
+				{
+					foreach (var media in tweet.Media)
+					{
+						url = media.MediaURLHttps;
+						if (url != null)
+						{
+							break;
+						}
+					}
+				}
+
+				Tuple<string, string> item = new Tuple<string, string>(text, url);
+				myCollection.Add(item);
+			}
+
+			UITableView _table;
+			_table = new UITableView
+
+			{
+				Frame = new CoreGraphics.CGRect(0, 65, View.Bounds.Width, View.Bounds.Height - 65),
+				Source = new TableSource(myCollection)
+			};
+
+			View.AddSubview(_table);
 		}
 
 		public override void DidReceiveMemoryWarning()
